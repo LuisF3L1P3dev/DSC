c961b4fec1f0b603cd8dba583cadfab7131d98f2
LuisF3L1P3dev <dantas.luis@escolar.ifrn.edu.br>
2026-09-05 20:34:04 -0300

    feat(calculadora): implementa subtracao com RequestParam

--- calculadora/src/main/java/br/edu/ifrn/calculadora/controllers/CalculadoraController.java
+++ calculadora/src/main/java/br/edu/ifrn/calculadora/controllers/CalculadoraController.java
@@ -3,6 +3,7 @@ package br.edu.ifrn.calculadora.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 // recebe requisições HTTP e resultado no corpo da resposta.
@@ -15,5 +16,11 @@ public class CalculadoraController {
     public int somar(@PathVariable int numero1, @PathVariable int numero2) {
         // Os valores entre chaves na URL chegam ao método por meio de @PathVariable.
         return numero1 + numero2;
+    }
+
+    @GetMapping("/subtrair")
+    public int subtrair(@RequestParam int numero1, @RequestParam int numero2) {
+        // @RequestParam recebe valores escritos após o sinal de interrogação da URL.
+        return numero1 - numero2;
     }
 }
